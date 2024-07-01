9f09f3b917959eb2ba7be80c8d91b74838b0729e
Mahir Shahriar <03mahirshahriar@gmail.com>
2024-07-01 21:08:59 +0600

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 import authRoutes from "./routes/auth_routes.js";
 import messageRoutes from "./routes/message_routes.js";
 import userRoutes from "./routes/user_routes.js";
 import connectDB from "./db/connect.js";
+
 import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
 app.use(express.json()); // to parse json data
 app.use(cookieParser());
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.use(
   cors({
@@ -29,6 +31,12 @@ app.use(
 app.use("/api/messages", messageRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectDB();
